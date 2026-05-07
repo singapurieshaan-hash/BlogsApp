fe365102bf6121275ba0c8e2c357023ec42a60ff
Eshaan <singapuri.eshaan@gmail,com>
2026-05-07 14:21:44 -0700

    Updated vercel.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hd#k^$u*xqdet^h869a&kr#_t55rbmdw)2sn#$or+w&$&47h-k'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
+'django-insecure-hd#k^$u*xqdet^h869a&kr#_t55rbmdw)2sn#$or+w&$&47h-k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".vercel.app",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.vercel.app",
