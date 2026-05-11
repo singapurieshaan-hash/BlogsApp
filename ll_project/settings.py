63d903e6eb1d0f3a95ef91105bb425e6ad1c596e
Eshaan <singapuri.eshaan@gmail,com>
2026-05-11 15:39:38 -0700

    Updated file.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+import dj_database_url 
 
 from pathlib import Path
 
@@ -87,10 +88,11 @@ WSGI_APPLICATION = 'll_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
